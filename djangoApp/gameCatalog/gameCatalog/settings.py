93cd47c7f65467ed7c8e6a92c1e4255e96689ac5
yoysung <91765271+yoysung@users.noreply.github.com>
2025-04-10 19:07:40 -0400

    Update settings.py
    Turned the sql requirement into a comment, this should help

--- djangoApp/gameCatalog/gameCatalog/settings.py
+++ djangoApp/gameCatalog/gameCatalog/settings.py
@@ -102,12 +102,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'gameCatalog.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+ #   'default': {
+   #     'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+   # }
+#}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
